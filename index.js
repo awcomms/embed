bcceb0a8a729a49ae30629d58a5dc0aea39bf3c6
Gold Edem Hogan <edge37@outlookcom>
2024-02-28 02:04:50 -1200

    .

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
-import {pipeline} from "@xenova/transformers"
+import { pipeline } from "@xenova/transformers";
 
-const app = new Koa()
+const app = new Koa();
 
-// Use bodyparser middleware to parse JSON request
 app.use(bodyParser({ enableTypes: ["text"] }));
 
 const embed = async (text) => {
   const extractor = await pipeline(
     "feature-extraction",
-    "Xenova/bert-large-cased-whole-word-masking"
+    "Xenova/bge-base-en-v1.5"
   );
   const { data } = await extractor(text, {
     pooling: "mean",
@@ -19,27 +18,20 @@ const embed = async (text) => {
   return Object.values(data);
 };
 
-
-// Define POST request route to handle the text
-app.use(async (ctx) => {
+app.post(async (ctx) => {
   try {
-    // Look for text property on request body
     const text = ctx.request.body;
     console.log(text);
     if (text) {
       const res = await embed(text);
-      console.log('embedding', embed)
-      // Check if the header "b" is present
+      console.log("embedding", embed);
       const b = ctx.request.header.b;
       if (b) {
-        // Return a Buffer of the embedding
         ctx.body = Buffer.from(res);
       } else {
-        // Return a json array of the embedding
         ctx.body = res;
       }
     } else {
-      // Send error if there's no text property on the body
       ctx.status = 400;
       ctx.body = { error: "Please provide text to process" };
     }
